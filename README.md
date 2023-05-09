6175acfa0a257e316a08fed319564aa1929613ce
Юсупов Роберт <r.yusupov@alean.ru>
2023-05-09 18:25:01 +0500

    init plugin

---
+++ README.md
@@ -0,0 +1,28 @@
+# IP Address Plugin
+
+## Описание
+Плагин IP Address позволяет отображать IP-адрес зарегистрированных пользователей в административной области WordPress. Он также сохраняет IP-адрес каждого пользователя при авторизации и хранит его в базе данных.
+
+## Использование
+1. Установите плагин, разместив его файлы в каталог плагинов вашего проекта WordPress.
+2. Активируйте плагин в разделе "Плагины" административной панели WordPress.
+3. После активации плагина, в административной области WordPress вы увидите новую колонку "IP" в таблице пользователей.
+4. В этой колонке будут отображаться IP-адреса зарегистрированных пользователей.
+
+## Лицензия
+Этот плагин распространяется под лицензией MIT. Подробности можно найти в файле `LICENSE`.
+
+## Поддержите автора плагина:
+- Оцените плагин на странице плагина в каталоге WordPress.
+- Оставьте положительный отзыв или комментарий на странице плагина.
+- Рассмотрите возможность пожертвования автору на странице плагина или пройдите по ссылке на его профиль.
+
+Ваша поддержка поможет автору продолжать поддерживать и улучшать плагин, а также создавать новые полезные инструменты для сообщества WordPress. Благодарим вас за вашу поддержку!
+
+## Support the Plugin Author:
+
+- Rate the plugin on the plugin page in the WordPress directory.
+- Leave a positive review or comment on the plugin page.
+- Consider making a donation to the author on the plugin page or visiting their profile.
+
+Your support will help the author to continue maintaining and improving the plugin, as well as creating new useful tools for the WordPress community. Thank you for your support!
---
+++ ip-plugin.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: IP Address Plugin
+Description: Display IP address of users in the admin area.
+Version: 1.0
+Author: TurkProgrammer
+Author URI: https://github.com/turkprogrammer
+*/
+
+// Add a new column to the users table in the admin area
+function ip_address_plugin_add_ip_column($columns) {
+    $columns['ip'] = 'IP';
+    return $columns;
+}
+add_filter('manage_users_columns', 'ip_address_plugin_add_ip_column');
+
+// Populate the new column with the IP address of each user
+function ip_address_plugin_display_ip($value, $column_name, $user_id) {
+    if ($column_name === 'ip') {
+        $user_ip = ip_address_plugin_get_user_ip($user_id);
+        $value = $user_ip ? $user_ip : 'N/A';
+    }
+    return $value;
+}
+add_action('manage_users_custom_column', 'ip_address_plugin_display_ip', 10, 3);
+
+// Function to get user's IP address
+function ip_address_plugin_get_user_ip($user_id) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'user_ips';
+
+    $user_ip = $wpdb->get_var($wpdb->prepare("SELECT ip FROM $table_name WHERE user_id = %d ORDER BY id DESC LIMIT 1", $user_id));
+
+    return $user_ip ? $user_ip : 'N/A';
+}
+
+
+// Create the user_ips table on plugin activation
+function ip_address_plugin_create_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'user_ips';
+
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE $table_name (
+            id bigint(20) NOT NULL AUTO_INCREMENT,
+            user_id bigint(20) NOT NULL,
+            ip varchar(45) NOT NULL,
+            timestamp datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (id),
+            KEY user_id (user_id)
+        ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+}
+register_activation_hook(__FILE__, 'ip_address_plugin_create_table');
+
+// Save user's IP address in the user_ips table on login
+function ip_address_plugin_save_user_ip($user_login, $user) {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'user_ips';
+
+    $user_ip = ip_address_plugin_get_user_ip($user->ID);
+    if ($user_ip !== 'N/A') {
+        $wpdb->insert($table_name, array(
+            'user_id' => $user->ID,
+            'ip' => $user_ip
+        ));
+    }
+}
+add_action('wp_login', 'ip_address_plugin_save_user_ip', 10, 2);
+
+
